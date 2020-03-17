b67f1481f9f31a5bd13fdccaca13f90989e1ccd3
Janne Kerola <kerola.janne@gmail.com>
2020-03-17 20:03:06 +0200

    Exercise 1.7

--- 1/unicafe/src/index.js
+++ 1/unicafe/src/index.js
@@ -26,6 +26,9 @@ const Statistics = ({ values }) => {
       <p>Good {values.good}</p>
       <p>Neutral {values.neutral}</p>
       <p>Bad {values.bad}</p>
+      <p>Total {values.total}</p>
+      <p>Average {values.average}</p>
+      <p>Positive {values.positive} %</p>
     </div>
   );
 };
@@ -35,8 +38,18 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
-  const values = { good, neutral, bad };
   const controls = { setGood, setNeutral, setBad };
+  const total = good + bad + neutral;
+  const average = (good - bad) / total || 0;
+  const positive = (good / total) * 100 || 0;
+  const values = {
+    good,
+    neutral,
+    bad,
+    total,
+    average,
+    positive,
+  };
   return (
     <div>
       <Header text="Give Feedback" />
