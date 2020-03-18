f62d2774abc7e58702580b21f45dad151f495c6b
Janne Kerola <kerola.janne@gmail.com>
2020-03-18 18:19:00 +0200

    Exercise 1.12*

--- 1/unicafe/src/index.js
+++ 1/unicafe/src/index.js
@@ -4,6 +4,15 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
+const anecdotes = [
+  'If it hurts, do it more often',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+];
+
 const Header = ({ text }) => {
   return (
     <h2>{text}</h2>
@@ -52,9 +61,31 @@ const Statistics = ({ values }) => {
     </table>
   );
 };
-
+const Quote = ({quoteControls}) => {
+  const { selected, setSelected } = quoteControls;
+  const random = () => {
+    let index = selected;
+    // while loop prevents duplicates in a row
+    while (selected === index) {
+      index = Math.floor(Math.random() * anecdotes.length);
+    }
+    return index;
+  };
+  return (
+    <div>
+      <Header text="Inspirational Quote" />
+      <p>&quot;{anecdotes[selected]}&quot;</p>
+      <button>Like</button>
+      <button onClick={() => setSelected(random())}>Next Quote</button>
+    </div>
+  );
+};
 const App = () => {
+  // from https://fullstackopen.com/osa1/monimutkaisempi_tila_reactin_debuggaus
   // tallenna napit omaan tilaansa
+  const likes = new Array(anecdotes.length).fill(0);
+  const [selected, setSelected] = useState(0);
+  const quoteControls = { setSelected, selected, likes };
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
@@ -77,6 +108,7 @@ const App = () => {
         <ButtonGroup controls={controls} values={values} />
         <Header text="Statistics" />
         <p>No feedback given.</p>
+        <Quote quoteControls={quoteControls} />
       </div>
     );
   }
