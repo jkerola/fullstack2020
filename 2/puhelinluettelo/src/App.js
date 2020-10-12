5786076ca13f6645927480ec4bd839f3b4548211
Janne Kerola <kerola.janne@gmail.com>
2020-10-12 12:58:38 +0300

    Exercise 2.8

--- 2/puhelinluettelo/src/App.js
+++ 2/puhelinluettelo/src/App.js
@@ -3,24 +3,41 @@ import React, { useState } from 'react'
 // from example at https://fullstackopen.com/osa2/lomakkeiden_kasittely#tehtavat-2-6-2-10
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' },
-    { name: 'Teemu Pukki' }
+    {
+      name: 'Arto Hellas',
+      number: '040 123 4567'
+    },
+    {
+      name: 'Teemu Pukki',
+      number: '08 122 999'
+    }
   ])
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
   const handleNameChange = (event) => {
     setNewName(event.target.value)
+  }
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
   }
   const addNewPerson = (event) => {
     event.preventDefault()
     const names = persons.map(person => person.name)
-    if(names.includes(newName)) {
-      window.alert(`${newName} has already been added to the phonebook!`)
+    const numbers = persons.map(person => person.number)
+    if (names.includes(newName) || numbers.includes(newNumber)) {
+      window.alert(`${names.includes(newName)
+        ? newName
+        : newNumber} has already been added to the phonebook!`)
+      setNewName('')
+      setNewNumber('')
     } else {
       const newPerson = {
         name: newName,
+        number: newNumber
       }
       setPersons(persons.concat(newPerson))
       setNewName('')
+      setNewNumber('')
     }
   }
   return (
@@ -28,7 +45,8 @@ const App = () => {
       <h2>Phonebook</h2>
       <form onSubmit={addNewPerson}>
         <div>
-          name: <input value={newName} onChange={handleNameChange} />
+          name: <input value={newName} onChange={handleNameChange} /><br />
+          number: <input value={newNumber} onChange={handleNumberChange} />
         </div>
         <div>
           <button type="submit">add</button>
@@ -36,7 +54,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <div>
-        {persons.map(person => <p key={person.name}>{person.name}</p>)}
+        {persons.map(person => <p key={person.name}>{person.name} {person.number}</p>)}
       </div>
     </div>
   )
