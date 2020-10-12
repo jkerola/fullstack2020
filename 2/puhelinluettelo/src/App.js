d7c5b3548916179e2ae995f206c54d775a85c302
Janne Kerola <kerola.janne@gmail.com>
2020-10-12 12:32:23 +0300

    Exercise 2.6

---
+++ 2/puhelinluettelo/src/App.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react'
+
+// from example at https://fullstackopen.com/osa2/lomakkeiden_kasittely#tehtavat-2-6-2-10
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' },
+    { name: 'Teemu Pukki' }
+  ])
+  const [newName, setNewName] = useState('')
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+  const addNewPerson = (event) => {
+    event.preventDefault()
+    const newPerson = {
+      name: newName,
+    }
+    setPersons(persons.concat(newPerson))
+    setNewName('')
+  }
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addNewPerson}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <div>
+        {persons.map(person => <p key={person.name}>{person.name}</p>)}
+      </div>
+    </div>
+  )
+
+}
+
+export default App
