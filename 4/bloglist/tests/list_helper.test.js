d35bbadf2cad87bc903aa1012a89eebc8191293a
Janne Kerola <kerola.janne@gmail.com>
2020-10-20 19:57:42 +0300

    Exercise 4.5*

--- 4/bloglist/tests/list_helper.test.js
+++ 4/bloglist/tests/list_helper.test.js
@@ -1,5 +1,29 @@
 const listHelper = require('../utils/list_helper')
 
+const blogItem = {
+  title: "D&D Fridays",
+  author: "Gary Gygax",
+  url: "tomb-of-horrors.blogspot.com",
+  likes: 23,
+  id: "5f8d80dd71d291e839736a80"
+}
+const blogList = [
+  blogItem,
+  {
+    title: "React Fans",
+    author: "Zuckerberg",
+    url: "marks-amazing-blog.blogspot.com",
+    likes: 0,
+    id: "5f8d800f71d291e839736a7f"
+  },
+  {
+    title: "Meritoppila löytökiekot",
+    author: "Andre 3000",
+    url: "meritoppila-discgolf.blogspot.com",
+    likes: 702,
+    id: "5f8d9a33c094140feafc3f02"
+  }]
+
 // from example at
 // https://fullstackopen.com/osa4/sovelluksen_rakenne_ja_testauksen_alkeet#tehtavat-4-3-4-7
 test('dummy returns one', () => {
@@ -10,29 +34,6 @@ test('dummy returns one', () => {
 })
 
 describe('total likes', () => {
-  const blogItem = {
-    title: "D&D Fridays",
-    author: "Gary Gygax",
-    url: "tomb-of-horrors.blogspot.com",
-    likes: 23,
-    id: "5f8d80dd71d291e839736a80"
-  }
-  const blogList = [
-    blogItem,
-    {
-      title: "React Fans",
-      author: "Zuckerberg",
-      url: "marks-amazing-blog.blogspot.com",
-      likes: 0,
-      id: "5f8d800f71d291e839736a7f"
-    },
-    {
-      title: "Meritoppila löytökiekot",
-      author: "Andre 3000",
-      url: "meritoppila-discgolf.blogspot.com",
-      likes: 702,
-      id: "5f8d9a33c094140feafc3f02"
-    }]
   test('of empty list is zero', () => {
     const list = []
     const result = listHelper.totalLikes(list)
@@ -51,6 +52,30 @@ describe('total likes', () => {
     })
     expect(result).toBe(total)
   })
+})
 
-
+describe('best liked blog', () => {
+  test('of empty list equals null', () => {
+    const result = listHelper.favoriteBlog([])
+    expect(result).toBe(null)
+  })
+  test('of list of one item to be that item', () => {
+    const result = listHelper.favoriteBlog([blogItem])
+    expect(result).toEqual(blogItem)
+  })
+  test('of multiple items is correct', () => {
+    const result = listHelper.favoriteBlog(blogList)
+    expect(result).toEqual(blogList[2])
+  })
+  test('of multiple winners is correct', () => {
+    //expect first item of equal values
+    const modBlogItem = {...blogItem, likes: 702}
+    const doubledList = [...blogList, modBlogItem]
+    const result = listHelper.favoriteBlog(doubledList)
+    expect(result).toEqual(blogList[2]) // first of two items of equal value
+    const tripledList = [modBlogItem, ...doubledList]
+    const secondResult = listHelper.favoriteBlog(tripledList)
+    expect(secondResult).toEqual(modBlogItem) // first of three items of equal value
+  })
+  
 })
--- 4/bloglist/utils/list_helper.js
+++ 4/bloglist/utils/list_helper.js
@@ -8,8 +8,17 @@ const totalLikes = (list) => {
     return list.map(blog => blog.likes).reduce((current, next) => current + next)
   }
 }
+const favoriteBlog = (list) => {
+  if (list.length === 0) {
+    return null
+  } else {
+    const maxLikes = Math.max(...list.map(blog => blog.likes))
+    return list.find(blog => blog.likes === maxLikes)
+  }
+}
 
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
