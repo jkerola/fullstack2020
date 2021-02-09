ae1baef44efdac91bbc3b5e3399b801128bac095
Janne Kerola <kerola.janne@gmail.com>
2021-02-09 15:44:23 +0200

    Exercise 5.7*

--- 5/bloglist-frontend/src/components/Blog.js
+++ 5/bloglist-frontend/src/components/Blog.js
@@ -1,10 +1,35 @@
 /* eslint-disable react/prop-types */
-import React from 'react'
+import React, { useState } from 'react'
 
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} by {blog.author}
-  </div>
-)
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false)
+  const hideWhenVisible = {
+    display: visible ? 'none' : '',
+    cursor: 'pointer'
+  }
+  const showWhenVisible = {
+    display: visible ? '' : 'none',
+    padding: 10,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+  const toggleVisibility = () => {
+    setVisible(!visible)
+  }
 
+  return (
+    <div>
+      <div style={hideWhenVisible} onClick={toggleVisibility}>
+        {blog.title} by {blog.author}
+      </div>
+      <div style={showWhenVisible}>
+        {blog.title} by {blog.author} <button onClick={toggleVisibility}>hide</button>
+        <br />{blog.url}
+        <br />likes: {blog.likes} <button>like</button>
+        <br />added by {blog.user.name}
+      </div>
+    </div>
+  )
+}
 export default Blog
