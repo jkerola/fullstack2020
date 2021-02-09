54de8932d530a48ce6ec45b9bf6a8977db63cf4a
Janne Kerola <kerola.janne@gmail.com>
2021-02-09 16:32:58 +0200

    Exercise 5.9*

--- 5/bloglist-frontend/src/components/BlogCollection.js
+++ 5/bloglist-frontend/src/components/BlogCollection.js
@@ -4,6 +4,9 @@ import Blog from './Blog'
 const BlogCollection = (blogItemControls) => {
   const blogs = blogItemControls.blogs
   const likeBlogItem = blogItemControls.likeBlogItem
+  blogs.sort(function (a, b) {
+    return b.likes - a.likes
+  })
   return (
     <div>
       {blogs.map(blog =>
